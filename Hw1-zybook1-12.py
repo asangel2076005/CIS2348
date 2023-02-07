7133d2fb164f498e413ee46b26867813eac67c01
asangel2076005 <122799955+asangel2076005@users.noreply.github.com>
2023-02-06 19:48:26 -0600

    Delete Hw1-zybook1-12.py

--- Hw1-zybook1-12.py
+++
@@ -1,7 +0,0 @@
-# Angelo Angel (2076005)
-
-user_num = int(input())
-user_num_squared = user_num * user_num  # Bug here; fix it when instructed
-
-print(user_num_squared)  # Output formatting issue here; fix it when instructed
-
