99bc51bf3c5b3c6d6178dfe210ed672688721703
asangel2076005 <122799955+asangel2076005@users.noreply.github.com>
2023-03-07 11:35:30 -0600

    Update Zybook8-10.py

--- Homework 2/Zybook8-10.py
+++ Homework 2/Zybook8-10.py
@@ -1,10 +1,10 @@
 # Angelo Angel (2076005)
 
 def palindrome(text):
-    text = text.lower()
-    text = ''.join(letter for letter in text if letter.isalnum())
+    edited_text = text.lower()
+    edited_text = ''.join(letter for letter in text if letter.isalnum())
 
-    if text == text[::-1]:
+    if edited_text == edited_text[::-1]:
         return print(f"{text} is a palindrome")
     else:
         return print(f"{text} is not a palindrome")
