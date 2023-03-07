aaab33155bc9db2c4bea55522e80a724a1b37c59
asangel2076005 <122799955+asangel2076005@users.noreply.github.com>
2023-03-07 11:38:46 -0600

    Update Zybook9-10.py

--- Homework 2/Zybook9-10.py
+++ Homework 2/Zybook9-10.py
@@ -12,3 +12,10 @@ with open(file_name, "r") as csvfile:
         for word in row:
             if word not in file_name_list:
                 file_name_list.append(f"{word} {row.count(word)}")
+
+for item in file_name_list:
+    if item not in unique_name_list:
+        unique_name_list.append(item)
+
+for item in unique_name_list:
+    print(item)                
