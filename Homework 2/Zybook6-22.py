b7090b13ff5679fa5c9d543de84659dab4c82e75
asangel2076005 <122799955+asangel2076005@users.noreply.github.com>
2023-03-07 11:28:18 -0600

    Create Zybook6-22.py

---
+++ Homework 2/Zybook6-22.py
@@ -0,0 +1,19 @@
+# Angelo Angel 2076005
+
+# Read in first equation, ax + by = c
+a = int(input())
+b = int(input())
+c = int(input())
+
+# Read in second equation, dx + ey = f
+d = int(input())
+e = int(input())
+f = int(input())
+
+check = False
+
+for x in range(-10, 10):
+    for y in range(-10, 10):
+        if ((a * x) + (b * y) == c) and ((d * x) + (e * y) == f):
+            check = True
+            print(f"{x} {y}")
