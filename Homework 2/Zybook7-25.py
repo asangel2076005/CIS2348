a0bf5a7f42b4f8cf05b7377c78e0820d41dcee48
asangel2076005 <122799955+asangel2076005@users.noreply.github.com>
2023-03-07 11:32:23 -0600

    Update Zybook7-25.py

--- Homework 2/Zybook7-25.py
+++ Homework 2/Zybook7-25.py
@@ -23,3 +23,31 @@ def exact_change(user_change):
 if __name__ == '__main__':
     input_val = int(input())
     num_dollars, num_quarters, num_dimes, num_nickels, num_pennies = exact_change(input_val)
+    
+    if input_val <= 0:
+        print("no change")
+    else: # input_val is greater than 0
+        if num_dollars > 1:
+            print(f"{num_dollars} dollars")
+        elif num_dollars == 1:
+            print("1 dollar")
+
+        if num_quarters > 1:
+            print(f"{num_quarters} quarters")
+        elif num_quarters == 1:
+            print("1 quarter")
+
+        if num_dimes > 1:
+            print(f"{num_dimes} dimes")
+        elif num_dimes == 1:
+            print("1 dime")
+
+        if num_nickels > 1:
+            print(f"{num_nickels} nickels")
+        elif num_nickels == 1:
+            print("1 nickel")
+
+        if num_pennies > 1:
+            print(f"{num_pennies} pennies")
+        elif num_pennies == 1:
+            print("1 penny")
