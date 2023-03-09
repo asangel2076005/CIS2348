a1bfa1c9735c732c3ce1f2b38273dac0f0606dd8
asangel2076005 <asangel@cougarnet.uh.edu>
2023-03-09 07:35:29 -0600

    Update 10-17

--- Homework 3/Zybook10-17.py
+++ Homework 3/Zybook10-17.py
@@ -11,6 +11,13 @@ class ItemToPurchase:
         return (f"{self.item_name} {self.item_quantity} @ ${self.item_price:.0f} = ${self.item_quantity * self.item_price:.0f}")
 
 
+def userQuery():
+    item_name = input("Enter the item name:\n")
+    item_price = float(input("Enter the item price:\n"))
+    item_quantity = int(input("Enter the item quantity:\n"))
+    return item_name, item_price, item_quantity
+
+
 if __name__ == "__main__":
     items = []
     for i in range(1, 3):
@@ -19,3 +26,13 @@ if __name__ == "__main__":
         user = ItemToPurchase(user_item_name, user_item_price, user_item_quantity)
         items.append(user)
         print()
+
+    print("TOTAL COST")
+    for item in items:
+        print(item.print_item_cost())
+
+    print()
+    total = 0
+    for item in items:
+        total = total + (item.item_price * item.item_quantity)
+    print(f"Total: ${total:.0f}")
