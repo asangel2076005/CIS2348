a3bbfb24782494049ef2a881ef16d3f799f2f8d7
asangel2076005 <asangel@cougarnet.uh.edu>
2023-03-11 20:22:10 -0600

    Updated Homework 3

--- Homework 3/Zybook11-27.py
+++ Homework 3/Zybook11-27.py
@@ -15,18 +15,20 @@ for jersey_number, jersey_rating in jersey_roster_sorted:
     print(f"Jersey number: {jersey_number}, Rating: {jersey_rating}")
 
 while True:
-    print("\nMENU")
+    print()
+    print("MENU")
     print("a - Add player")
     print("d - Remove player")
     print("u - Update player rating")
     print("r - Output players above a rating")
     print("o - Output roster")
-    print("q - Quit\n")
+    print("q - Quit")
+    print()
 
     user_choice = input("Choose an option:\n")
 
     if user_choice == "o":
-        print("ROSTER")
+        print("\nROSTER")
         for jersey_number, jersey_rating in jersey_roster_sorted:
             print(f"Jersey number: {jersey_number}, Rating: {jersey_rating}")
     elif user_choice == "a":
@@ -34,3 +36,27 @@ while True:
         jersey_rating = int(input("Enter the player's rating:\n"))
         jersey_roster[jersey_number] = jersey_rating
         jersey_roster_sorted = sorted(jersey_roster.items())
+        print("\nPlayer added.")
+    elif user_choice == "d":
+        jersey_number = int(input("Enter a jersey number:\n"))
+        if jersey_number in jersey_roster:
+            del jersey_roster[jersey_number]
+            jersey_roster_sorted = sorted(jersey_roster.items())
+        else:
+            print("\nThat player is not in the roster.")
+    elif user_choice == "u":
+        jersey_number = int(input("Enter a jersey number:\n"))
+        if jersey_number in jersey_roster:
+            jersey_rating = int(input("Enter a new rating for player:\n"))
+            jersey_roster[jersey_number] = jersey_rating
+            jersey_roster_sorted = sorted(jersey_roster.items())
+        else:
+            print("\nThat player is not in the roster.")
+    elif user_choice == "r":
+        rating = int(input("Enter a rating:\n"))
+        print(f"\nABOVE {rating}")
+        for jersey_number, jersey_rating in jersey_roster_sorted:
+            if jersey_rating > rating:
+                print(f"Jersey number: {jersey_number}, Rating: {jersey_rating}")
+    elif user_choice == "q":
+        break
