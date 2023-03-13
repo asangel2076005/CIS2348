895ea1dfab41c5ea531fe8f4e356d10f2b894e95
asangel2076005 <asangel@cougarnet.uh.edu>
2023-03-12 21:10:25 -0500

    updated 10-17

--- Homework 3/Zybook10-17.py
+++ Homework 3/Zybook10-17.py
@@ -2,37 +2,34 @@
 
 class ItemToPurchase:
 
-    def __init__(self, item_name="none", item_price=0, item_quantity=0):
-        self.item_name = item_name
-        self.item_price = item_price
-        self.item_quantity = item_quantity
+    def __init__(self, name="none", price=0.0, quantity=0):
+        self.item_name = name
+        self.item_price = price
+        self.item_quantity = quantity
+
+    def get_item_cost(self):
+        item_cost = self.item_price * self.item_quantity
+        return item_cost
 
     def print_item_cost(self):
-        return (f"{self.item_name} {self.item_quantity} @ ${self.item_price:.0f} = ${self.item_quantity * self.item_price:.0f}")
-
-
-def userQuery():
-    item_name = input("Enter the item name:\n")
-    item_price = float(input("Enter the item price:\n"))
-    item_quantity = int(input("Enter the item quantity:\n"))
-    return item_name, item_price, item_quantity
+        print(f"{self.item_name} {self.item_quantity} @ ${self.item_price:.0f} = ${self.get_item_cost():.0f}")
 
 
 if __name__ == "__main__":
     items = []
-    for i in range(1, 3):
+    for i in range(1, 2+1):
         print(f"Item {i}")
-        user_item_name, user_item_price, user_item_quantity = userQuery()
-        user = ItemToPurchase(user_item_name, user_item_price, user_item_quantity)
-        items.append(user)
+        item_name = input(f"Enter the item name:\n")
+        item_price = float(input(f"Enter the item price:\n"))
+        item_quantity = int(input(f"Enter the item quantity:\n"))
         print()
+        item = ItemToPurchase(item_name, item_price, item_quantity)
+        items.append(item)
 
+    total_cost = 0
     print("TOTAL COST")
     for item in items:
-        print(item.print_item_cost())
-
+        item.print_item_cost()
+        total_cost = total_cost + item.get_item_cost()
     print()
-    total = 0
-    for item in items:
-        total = total + (item.item_price * item.item_quantity)
-    print(f"Total: ${total:.0f}")
+    print(f"Total: ${total_cost:.0f}")
