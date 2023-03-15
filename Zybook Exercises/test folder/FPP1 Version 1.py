12a4f7bc5967386adcf2a138da4d1f6700a0e8a2
asangel2076005 <asangel@cougarnet.uh.edu>
2023-03-15 16:47:32 -0500

    Updated input.py

--- Zybook Exercises/test folder/FPP1 Version 1.py
+++ Zybook Exercises/test folder/FPP1 Version 1.py
@@ -28,7 +28,7 @@ class CsvFiles:
         return list_of_items
 
     def sorted_list_of_items(self):
-        sorted_list_of_items = sorted(self.list_of_items(), key=sort_by_id)
+        sorted_list_of_items = sorted(self.list_of_items())
         return sorted_list_of_items
 
 
@@ -47,60 +47,44 @@ def sort_by_name_date(name_date):
 
 
 def sort_by_id(i_d):
-    return i_d[0]
+    return int(i_d[0])
 
 
 def sort_by_price(price):
     return int(price[3])
 
 
-# Combines all csv files and places all their contents with their respective ID's
-def combined_list(list1, list2, list3):
-    list1 = sorted(list1, key=sort_by_id)
-    list2 = sorted(list2, key=sort_by_id)
-    list3 = sorted(list3, key=sort_by_id)
-    general_list = []
-    for row_x, row_y, row_z in zip(list1, list2, list3):
-        for item_x, item_y, item_z in zip(row_x, row_y, row_z):
-            if item_x == item_y == item_z:
-                general_list.append([item_x, row_x[1], row_y[1], row_z[1]])
-    # Places items by order of: item id, manufacturer name, item type, price, service date, and situation
-    formatted_items = []
-    for row in general_list:
-        formatted_items.append([row[0], row[1][0], row[1][1], row[2][0], row[3][0], row[1][2]])
-    return formatted_items
-
-
-# Splits the mm/dd/yyyy format into their respective positions of date and convert each to int
-def get_current_date():
-    import datetime
-    current_date = datetime.date.today()
-    current_date_formatted = current_date.strftime("%m/%d/%Y")
-
-    year = int(current_date_formatted.split("/")[2])
-    day = int(current_date_formatted.split("/")[1])
-    month = int(current_date_formatted.split("/")[0])
-    return month, day, year
-
-
 if __name__ == "__main__":
     import csv
+    import datetime
 
     # ManufacturerList.csv
     manufacturer_list = CsvFiles()
-    manufacturer_list.file_name = input("Enter manufacturer list file name:\n")
+    manufacturer_list.file_name = "ManufacturerList.csv"  # input("Enter manufacturer list file name:\n")
 
     # PriceList.csv
     price_list = CsvFiles()
-    price_list.file_name = input("Enter price list file name:\n")
+    price_list.file_name = "PriceList.csv"  # input("Enter price list file name:\n")
 
     # ServiceDatesList.csv
     service_dates_list = CsvFiles()
-    service_dates_list.file_name = input("Enter service dates list file name:\n")
+    service_dates_list.file_name = "ServiceDatesList.csv"  # input("Enter service dates list file name:\n")
 
-    full_inventory = combined_list(manufacturer_list.sorted_list_of_items(),
-                                   price_list.sorted_list_of_items(),
-                                   service_dates_list.list_of_items())
+    # Combines all csv files and places all their contents with their respective ID's
+    combined_list = zip(manufacturer_list.sorted_list_of_items(),
+                        price_list.sorted_list_of_items(),
+                        service_dates_list.sorted_list_of_items())
+    full_inventory_list = []
+    for row_x, row_y, row_z in combined_list:
+        for item_x, item_y, item_z in zip(row_x, row_y, row_z):
+            if item_x == item_y == item_z:
+                full_inventory_list.append([item_x, row_x[1], row_y[1], row_z[1]])
+
+    # Places items by order of: item id, manufacturer name, item type, price, service date, and situation
+    # !IMPORTANT USE full_inventory AS BASE FOR OTHER CSV FILES
+    full_inventory = []
+    for row in full_inventory_list:
+        full_inventory.append([row[0], row[1][0], row[1][1], row[2][0], row[3][0], row[1][2]])
 
     # Writes FullInventory.csv File
     with open("FullInventory.csv", "w", newline="") as full_inventory_file:
@@ -143,8 +127,16 @@ if __name__ == "__main__":
         tower_inventory_writer = csv.writer(tower_inventory_file)
         tower_inventory_writer.writerows(tower_inventory_by_id)
 
+    # This section encompasses allowed dates that take place before today's date
+    current_date = datetime.date.today()
+    current_date_formatted = current_date.strftime("%m/%d/%Y")
+
+    # Splits the mm/dd/yyyy format into their respective positions of date and convert each to int
+    current_year = int(current_date_formatted.split("/")[2])
+    current_day = int(current_date_formatted.split("/")[1])
+    current_month = int(current_date_formatted.split("/")[0])
+
     # Stores past service dates based on today's date into a new list
-    current_month, current_day, current_year = get_current_date()
     past_service_date = []
     for item in full_inventory:
         month = int(item[4].split("/")[0])
--- Zybook Exercises/test folder/FPP1 Version 2.py
+++ Zybook Exercises/test folder/FPP1 Version 2.py
@@ -28,7 +28,7 @@ class CsvFiles:
         return list_of_items
 
     def sorted_list_of_items(self):
-        sorted_list_of_items = sorted(self.list_of_items())
+        sorted_list_of_items = sorted(self.list_of_items(), key=sort_by_id)
         return sorted_list_of_items
 
 
@@ -54,37 +54,53 @@ def sort_by_price(price):
     return int(price[3])
 
 
+# Combines all csv files and places all their contents with their respective ID's
+def combined_list(list1, list2, list3):
+    list1 = sorted(list1, key=sort_by_id)
+    list2 = sorted(list2, key=sort_by_id)
+    list3 = sorted(list3, key=sort_by_id)
+    general_list = []
+    for row_x, row_y, row_z in zip(list1, list2, list3):
+        for item_x, item_y, item_z in zip(row_x, row_y, row_z):
+            if item_x == item_y == item_z:
+                general_list.append([item_x, row_x[1], row_y[1], row_z[1]])
+    # Places items by order of: item id, manufacturer name, item type, price, service date, and situation
+    formatted_items = []
+    for row in general_list:
+        formatted_items.append([row[0], row[1][0], row[1][1], row[2][0], row[3][0], row[1][2]])
+    return formatted_items
+
+
+# Splits the mm/dd/yyyy format into their respective positions of date and convert each to int
+def get_current_date():
+    import datetime
+    current_date = datetime.date.today()
+    current_date_formatted = current_date.strftime("%m/%d/%Y")
+
+    year = int(current_date_formatted.split("/")[2])
+    day = int(current_date_formatted.split("/")[1])
+    month = int(current_date_formatted.split("/")[0])
+    return month, day, year
+
+
 if __name__ == "__main__":
     import csv
-    import datetime
 
     # ManufacturerList.csv
     manufacturer_list = CsvFiles()
-    manufacturer_list.file_name = input("Enter manufacturer list file name:\n")
+    manufacturer_list.file_name = "ManufacturerList.csv"  # input("Enter manufacturer list file name:\n")
 
     # PriceList.csv
     price_list = CsvFiles()
-    price_list.file_name = input("Enter price list file name:\n")
+    price_list.file_name = "PriceList.csv"  # input("Enter price list file name:\n")
 
     # ServiceDatesList.csv
     service_dates_list = CsvFiles()
-    service_dates_list.file_name = input("Enter service dates list file name:\n")
+    service_dates_list.file_name = "ServiceDatesList.csv"  # input("Enter service dates list file name:\n")
 
-    # Combines all csv files and places all their contents with their respective ID's
-    combined_list = zip(manufacturer_list.sorted_list_of_items(),
-                        price_list.sorted_list_of_items(),
-                        service_dates_list.sorted_list_of_items())
-    full_inventory_list = []
-    for row_x, row_y, row_z in combined_list:
-        for item_x, item_y, item_z in zip(row_x, row_y, row_z):
-            if item_x == item_y == item_z:
-                full_inventory_list.append([item_x, row_x[1], row_y[1], row_z[1]])
-
-    # Places items by order of: item id, manufacturer name, item type, price, service date, and situation
-    # !IMPORTANT USE full_inventory AS BASE FOR OTHER CSV FILES
-    full_inventory = []
-    for row in full_inventory_list:
-        full_inventory.append([row[0], row[1][0], row[1][1], row[2][0], row[3][0], row[1][2]])
+    full_inventory = combined_list(manufacturer_list.sorted_list_of_items(),
+                                   price_list.sorted_list_of_items(),
+                                   service_dates_list.list_of_items())
 
     # Writes FullInventory.csv File
     with open("FullInventory.csv", "w", newline="") as full_inventory_file:
@@ -127,16 +143,8 @@ if __name__ == "__main__":
         tower_inventory_writer = csv.writer(tower_inventory_file)
         tower_inventory_writer.writerows(tower_inventory_by_id)
 
-    # This section encompasses allowed dates that take place before today's date
-    current_date = datetime.date.today()
-    current_date_formatted = current_date.strftime("%m/%d/%Y")
-
-    # Splits the mm/dd/yyyy format into their respective positions of date and convert each to int
-    current_year = int(current_date_formatted.split("/")[2])
-    current_day = int(current_date_formatted.split("/")[1])
-    current_month = int(current_date_formatted.split("/")[0])
-
     # Stores past service dates based on today's date into a new list
+    current_month, current_day, current_year = get_current_date()
     past_service_date = []
     for item in full_inventory:
         month = int(item[4].split("/")[0])
@@ -172,7 +180,3 @@ if __name__ == "__main__":
         damaged_inventory_by_price = sorted(damaged_inventory, key=sort_by_price, reverse=True)
         damaged_inventory_writer = csv.writer(damaged_inventory_file)
         damaged_inventory_writer.writerows(damaged_inventory_by_price)
-
-
-
-
