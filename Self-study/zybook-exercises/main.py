24468c2e45c8e3f38422f78aa0213e3a16372e57
asangel2076005 <asangel@cougarnet.uh.edu>
2023-04-06 15:35:35 -0500

    Moved files within Self Study accordingly

---
+++ Self-study/zybook-exercises/main.py
@@ -0,0 +1,6 @@
+def sort_by_name(name):
+    return name[1]
+
+if name = main
+
+    full_inventory_by_dates = sorted(full_inventory, key=sort_by_name)
--- Zybook Exercises/main.py
+++
@@ -1,7 +0,0 @@
-def jiffies_to_second(jiffy):
-    return jiffy / 100
-
-
-if __name__ == '__main__':
-    user_jiffy = float(input())
-    print(f"{jiffies_to_second(user_jiffy):.3f}")
